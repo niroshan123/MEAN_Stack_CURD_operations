68706f401259f0c1ef8272080a98b03c65aef71a
Niroshan07 <Niroshan07@bitbucket.com>
2019-08-14 02:55:57 +0530

    First commit

---
+++ CurdNodejs/index.js
@@ -0,0 +1,11 @@
+const express=require('express');
+const bodyParser=require('body-parser');
+
+const {mongoose}=require('./db.js');
+
+var app=express();
+app.use(bodyParser.json());
+
+app.listen(3008,()=>{
+    console.log('Server started at port : 3000')
+})
