134c7a312a8ee9c8bad7ed0e64d0539c02a8e336
Niroshan07 <Niroshan07@bitbucket.com>
2019-08-20 07:55:34 +0530

    CURD opperations 1

--- CurdNodejs/index.js
+++ CurdNodejs/index.js
@@ -1,11 +1,13 @@
 const express=require('express');
-const bodyParser=require('body-parser');
+const bodyParser=require('body-parser');//ppackage imports
 
-const {mongoose}=require('./db.js');
+const {mongoose}=require('./db.js');//local import
+var employeeController = require('./controllers/employeeControllers.js');//request for employeeController variable
 
-var app=express();
-app.use(bodyParser.json());
+var app=express();//working with express package
+app.use(bodyParser.json());//send json data to node project
 
-app.listen(3008,()=>{
+app.listen(3000,()=>{
     console.log('Server started at port : 3000')
-})
+});//starting the express seerver
+app.use('/employees',employeeController);//in order to add routers from employeeController to this application
