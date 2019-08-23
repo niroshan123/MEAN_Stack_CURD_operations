7837d6ecca8fd1be3fa8d40448851853062b5f9a
Niroshan07 <Niroshan07@bitbucket.com>
2019-08-23 09:22:41 +0530

    Final Commit

--- CurdAngular/src/app/employee/employee.component.ts
+++ CurdAngular/src/app/employee/employee.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+
+
 @Component({
   selector: 'app-employee',
   templateUrl: './employee.component.html',
--- CurdNodejs/index.js
+++ CurdNodejs/index.js
@@ -1,13 +1,34 @@
+// const express = require('express');
+// const bodyParser = require('body-parser');
+// const cors = require('cors');
+
+// const { mongoose } = require('./db.js');
+// var employeeController = require('./controllers/employeeController.js');
+
+// var app = express();
+// app.use(bodyParser.json());
+// app.use(cors({ origin: 'http://localhost:4200' }));
+
+// app.listen(3000, () => console.log('Server started at port : 3000'));
+
+
+// app.use('/employees', employeeController);
+
 const express=require('express');
 const bodyParser=require('body-parser');//ppackage imports
+const cors = require('cors');
 
-const {mongoose}=require('./db.js');//local import
-var employeeController = require('./controllers/employeeControllers.js');//request for employeeController variable
+const { mongoose }=require('./db.js');//local import
+var employeeController = require('./controllers/employeeController.js');
+//request for employeeController variable
 
 var app=express();//working with express package
 app.use(bodyParser.json());//send json data to node project
 
-app.listen(3000,()=>{
-    console.log('Server started at port : 3000')
-});//starting the express seerver
-app.use('/employees',employeeController);//in order to add routers from employeeController to this application
+app.use(cors({ origin: 'http://localhost:4200' }));
+
+
+app.listen(3000,()=>
+    console.log('Server started at port : 3000'));
+//starting the express seerver
+app.use('/employees', employeeController);//in order to add routers from employeeController to this application
