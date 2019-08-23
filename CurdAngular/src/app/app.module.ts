eaef5429b4143188cf40366b02acbbc07f67863a
Niroshan07 <Niroshan07@bitbucket.com>
2019-08-23 11:38:01 +0530

    Final Commit

--- CurdAngular/src/app/app.module.ts
+++ CurdAngular/src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EmployeeComponent } from './employee/employee.component';
+
 
 @NgModule({
   declarations: [
@@ -12,7 +14,8 @@ import { EmployeeComponent } from './employee/employee.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
