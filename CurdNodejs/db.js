ae41b0fa2ea867027c995ba2f8c725af9d09a564
Niroshan07 <Niroshan07@bitbucket.com>
2019-08-23 17:42:26 +0530

    mLab DB

--- CurdNodejs/db.js
+++ CurdNodejs/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/CrudDB', (err) => {
+mongoose.connect('mongodb+srv://Niroshan:niro123@niroshan-hlz86.mongodb.net/test?retryWrites=true&w=majority', (err) => {
     if (!err)
         console.log('MongoDB connection succeeded.');
     else
